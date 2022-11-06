4a406591a44b8f5717acc2b331b0d91682fcb545
BORIS\Boris <b.zivkovic97@yahoo.com>
2022-11-06 22:59:49 +0100

    Successfuly singed up with the email and password using firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,30 +1,100 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase.utils';
 
 import './sign-in.styles.css';
 
+const defaultInputFields = {
+  email: '',
+  password: '',
+  confirmPassword: ''
+}
+
 const SignIn = () => {
+  const [ isSigningUp, setIsSignUp ] = useState('false');
+  const [inputFields, setInputFields] = useState(defaultInputFields);
+  const { email, password, confirmPassword } = inputFields;
   
+  const handleSignUp = () => {
+    setIsSignUp(!isSigningUp);
+  }
+
+  const resetInputFields = () => {
+    setInputFields(defaultInputFields);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setInputFields({ ...inputFields, [name]: value });
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if(password !== confirmPassword) {
+      alert("Passwords don't match!");
+      return;
+    }
+
+    try {
+      const {user} = await createAuthUserWithEmailAndPassword(email, password);
+      console.log('creating new user')
+      await createUserDocumentFromAuth( user, { email });
+      resetInputFields();
+    } catch(error) {
+      console.log(error.code); 
+    }
+  }
+
 
   return (
     <div className='sign-in-container'>
+      <span>Sign up</span>
       <div className='username-pssw-input-container'>
-        <input
-          className='username-input'
-          type='text'
-          placeholder='Enter username' 
-        />
-        <input
-          className='password-input'
-          type='password'
-          placeholder='Enter password'
-        />
-      </div>
-      <div className='sign-in-up-buttons-container'>
-        <button className='sign-in-button'>LOG IN</button>
-        {/* <button className='sign-up-button'>SIGN UP</button> */}
+        <form onSubmit={handleSubmit}>
+          <input
+            required
+            name='email'
+            label='email'
+            className='username-input'
+            type='email'
+            placeholder='Enter username'
+            onChange={handleChange}
+            value={email}
+          />
+          <input
+            required
+            name='password'
+            className='password-input'
+            type='password'
+            placeholder='Enter password'
+            onChange={handleChange}
+            value={password}
+          />
+          <input
+            required
+            name='confirmPassword'
+            className='password-input'
+            type='password'
+            placeholder='Confirm your password'
+            onChange={handleChange}
+            value={confirmPassword}
+          />
+          <button 
+            className='sign-up-button'
+            type='submit'
+          >
+            SIGN UP
+          </button>
+        </form>
       </div>
       <button className='sign-in-with-google-button'>SIGN IN WITH GOOGLE</button>
-      <span className='no-account-span'>Don't have an account?</span>
+      <span 
+        className='no-account-span'
+        onClick={handleSignUp}
+        >Already have an account?
+      </span>
     </div>
   )
 }
